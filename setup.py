5ed26d49a7fea6987778f076377f89192a2f1d50
Kei Nakayama <nakayama@chiba-data.co.jp>
2021-11-10 16:52:22 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='str2fix',
+    version='0.1.0',
+    description='A example Python package',
+    url='https://github.com/',
+    author='kexi',
+    author_email='kexi@kexi.jp',
+    license='MIT',
+    packages=['str2fix'],
+    install_requires=['mojimoji',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
---
+++ str2fix/__init__.py
@@ -0,0 +1,130 @@
+"""
+str2fix.
+
+An example python library.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'kexi'
+__credits__ = 'kexi'
+
+import mojimoji as moji
+
+
+def convert_with_x(val: str, field_length: int) -> str:
+    """半角にして左詰(残りは半角スペース)
+
+    :param val: 文字列
+    :type val: str
+    :param field_length: フィールド長
+    :type field_length: int
+    :return: 固定長文字列(UTF-8)
+    :rtype: str
+    """
+
+    val = moji.zen_to_han(val)
+    byte = 0
+    r = ""
+    for ch in val:
+        r += ch
+        byte += len(ch.encode('cp932'))
+        if byte >= field_length:
+            break
+
+    while byte < field_length:
+        r += " "
+        byte += 1
+
+    return r
+
+
+def convert_with_n(val: str, field_length: int) -> str:
+    """全角にして左詰(残りは全角スペース)
+
+    :param val: 文字列
+    :type val: str
+    :param field_length: フィールド長
+    :type field_length: int
+    :return: 固定長文字列(UTF-8)
+    :rtype: str
+    """
+    val = moji.han_to_zen(val)
+    byte = 0
+    r = ""
+    for ch in val:
+        r += ch
+        byte += len(ch.encode('cp932'))
+        if byte >= field_length:
+            break
+
+    while byte < field_length:
+        r += "　"
+        byte += 2
+
+    return r
+
+
+def convert_with_m(val: str, field_length: int) -> str:
+    """左詰して残りは半角スペース(半角・全角混在可)
+
+    :param val: 文字列
+    :type val: str
+    :param field_length: フィールド長
+    :type field_length: int
+    :return: 固定長文字列(UTF-8)
+    :rtype: str
+    """
+
+    byte = 0
+    r = ""
+    for ch in val:
+        ch_len = len(ch.encode('cp932'))
+        if ch_len + byte <= field_length:
+            r += ch
+            byte += ch_len
+        else:
+            r += ' '
+            byte += 1
+
+        if byte >= field_length:
+            break
+
+    while byte < field_length:
+        r += " "
+        byte += 1
+
+    return r
+
+
+def convert_with_9(val: str, field_length: int) -> str:
+    """前ゼロ(半角)をつけて右詰
+
+    :param val: 文字列
+    :type val: str
+    :param field_length: フィールド長
+    :type field_length: int
+    :return: 固定長文字列(UTF-8)
+    :rtype: str
+    """
+    val = moji.zen_to_han(val)
+    r = field_length * '0' + val
+    r = r[-field_length:]
+
+    return r
+
+
+def convert_with_z(val: str, field_length: int) -> str:
+    """前スペース(半角)をつけて右詰
+
+    :param val: 文字列
+    :type val: str
+    :param field_length: フィールド長
+    :type field_length: int
+    :return: 固定長文字列(UTF-8)
+    :rtype: str
+    """
+    val = moji.zen_to_han(val)
+    r = field_length * ' ' + val
+    r = r[-field_length:]
+
+    return r
